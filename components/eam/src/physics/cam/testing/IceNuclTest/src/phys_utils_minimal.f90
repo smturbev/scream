682c2cbf49cc68cb6d3ab13b0076c32470e6c713
Sami Turbeville <smturbev@uw.edu>
2023-06-01 14:22:53 -0600

    added NumCirrusSulf = 20 for nsulf vals

--- components/eam/src/physics/cam/testing/IceNuclTest/src/phys_utils_minimal.f90
+++ components/eam/src/physics/cam/testing/IceNuclTest/src/phys_utils_minimal.f90
@@ -4,4 +4,8 @@ module physics_utils
   integer,parameter,public :: rtype8 = selected_real_kind(15, 307) ! 8 byte real, compatible with c type double
   real,parameter,public ::  NumCirrusINP = 2.e-3 !in units per cm3! BG added some reasonable background upper tropospheric dust/het ice nuclei value for cirrus conditions
   !BG: based on simulated upper tropospheric (200 hPa) values of dust in Tropical Western Pacific for ECHAM-HAM GCM simulations
+  real,public :: NumCirrusSulf = 20. !20 will effectively limit the ice nucleation for one event to 20 k /L: better limit, as no way to deplete air mass of nsulf in this code
+  !100. !100/cm3 
+  !BG max number of sulphate aerosol used for homog freezing -> taken some reasonable value for upper troposphere
+  !used this number as Liu and Penner, 2005
 end module physics_utils
